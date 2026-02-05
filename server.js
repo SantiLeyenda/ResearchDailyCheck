3a06f9afc6c356c5b7ba41b8c539184f35851228
santiLeyenda <a01643411@tec.mx>
2026-02-05 16:35:32 -0600

    Use dynamic PORT for deployment

--- server.js
+++ server.js
@@ -30,4 +30,5 @@ app.use((err, req, res, next) => {
 })
 
 
-app.listen(3000, () => console.log("Running on 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Running on ${PORT}`));
